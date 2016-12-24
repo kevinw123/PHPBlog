ed9e8f9b36e41962b176676cf50e0f51dc173c70
kevinw123 <kevinwong_895@hotmail.com>
2016-12-24 14:33:45 -0800

    Shortening text on index page

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php include 'config/config.php'; ?>
 <?php include 'libraries/Database.php'; ?>
 <?php include 'includes/header.php'; ?>
+<?php include 'helpers/format_helper.php'; ?>
 <?php
   // Create DB object
   $db = new Database();
@@ -16,9 +17,9 @@
   <?php while($row = $posts->fetch_assoc()) : ?>
     <div class="blog-post">
       <h2 class="blog-post-title"><?php echo $row['title']; ?></h2>
-      <p class="blog-post-meta"><?php echo $row['date']; ?> by <a href="#"><?php echo $row['author']; ?></a></p>
-      <p><?php echo $row['body']; ?></p>
-      <a class="readmore" href="post.php?id=1">Read More</a>
+      <p class="blog-post-meta"><?php echo formatDate($row['date']); ?> by <a href="#"><?php echo $row['author']; ?></a></p>
+      <p><?php echo shortenText($row['body']); ?></p>
+      <a class="readmore" href="<?php echo urlencode($row['id']); ?>">Read More</a>
     </div><!-- /.blog-post -->
 
 <?php endwhile; ?>
