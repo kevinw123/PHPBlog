f1797ee34f09081b19de075461cd7899436abe7b
kevinw123 <kevinwong_895@hotmail.com>
2016-12-23 23:22:32 -0800

    adding update, insert, delete for database

--- libraries/Database.php
+++ libraries/Database.php
@@ -23,4 +23,52 @@ class Database{
 		}
 	}
 
+	// Select
+	public function select($query){
+		$result = $this->link->query($query) or die($this->link->error.__LINE__);
+		if($result->num_rows > 0){
+			return $result;
+		} else {
+			return false;
+		}
+	}
+
+	// Insert 
+	public function insert($query){
+		$insert_row = $this->link->query($query) or die($this->link->error.__LINE__);
+
+		// Validate insert
+		if($insert_row){
+			header("Location: index.php?msg=".urlencode('Record Added'));
+			exit();
+		} else {
+			die('Error: ('. $this->link->errno .') '. $this->link->error);
+		}
+	}
+
+	// Update
+	public function update($query){
+		$update_row = $this->link->query($query) or die($this->link->error.__LINE__);
+
+		// Validate update
+		if($update_row){
+			header("Location: index.php?msg=".urlencode('Record Updated'));
+			exit();
+		} else {
+			die('Error: ('. $this->link->errno .') '. $this->link->error);
+		}
+	}
+
+	// Delete
+	public function delete($query){
+		$delete_row = $this->link->query($query) or die($this->link->error.__LINE__);
+
+		// Validate update
+		if($delete_row){
+			header("Location: index.php?msg=".urlencode('Record Deleted'));
+			exit();
+		} else {
+			die('Error: ('. $this->link->errno .') '. $this->link->error);
+		}
+	}
 }
